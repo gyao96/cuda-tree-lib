4926525cf53c7123964ac17891a19747273b0f2f
ZXCroon <zxcroon@gmail.com>
2020-04-17 17:50:23 -0400

    radix tree; cuda

--- src/tree/radix_tree.cu
+++ src/tree/radix_tree.cu
@@ -2,55 +2,80 @@
 #include <ctime>
 #include <algorithm>
 #include <iostream>
+#include "common.h"
+
+#define N_THREADS_PER_BLK 256
 
 
 class RadixTreeNode {
 public:
     RadixTreeNode *left, *right, *parent;
     int val;
-    bool isLeaf() { return left == nullptr && right == nullptr; }
+    __host__ __device__ bool isLeaf() { return left == nullptr && right == nullptr; }
 };
 
 
 class RadixTree {
 public:
     RadixTreeNode *root;
-    RadixTree(int *buf, int count);
-    ~RadixTree();
-    void insert(int x);
-    void construct(int *buf);
-    void print();
-    bool check() { return check(root, 0, count - 1); }
+    __host__ __device__ void init(int count);
+    __host__ __device__ void construct(int *buf);
+    __host__ __device__ void destroy();
+    __host__ void print();
+    __host__ __device__ bool check() { return check(root, 0, count - 1); }
 private:
     int count;
     RadixTreeNode *internals, *leaves;
-    int lcp(int x, int y);
-    bool check(RadixTreeNode *p, int l, int r);
-    void printNode(RadixTreeNode *p);
+    __host__ __device__ int lcp(int x, int y);
+    __host__ __device__ bool check(RadixTreeNode *p, int l, int r);
+    __host__ void printNode(RadixTreeNode *p);
 };
 
 
-RadixTree::RadixTree(int *buf, int count) : count(count) {
-    internals = new RadixTreeNode[count - 1];
-    leaves = new RadixTreeNode[count];
-    for (int i = 0; i < count; ++i) {
-        leaves[i].val = buf[i];
-        leaves[i].left = leaves[i].right = nullptr;
+void RadixTree::init(int count) {
+#ifdef __CUDA_ARCH__
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+#else
+    int tid = 0;
+#endif
+    if (tid == 0) {
+        this->count = count;
+        internals = new RadixTreeNode[count - 1];
+        leaves = new RadixTreeNode[count];
+        root = &internals[0];
+        root->parent = nullptr;
     }
-    internals[0].parent = nullptr;
-    construct(buf);
 }
 
 
-RadixTree::~RadixTree() {
-    delete[] internals;
-    delete[] leaves;
+void RadixTree::destroy() {
+#ifdef __CUDA_ARCH__
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+#else
+    int tid = 0;
+#endif
+    if (tid == 0) {
+        delete[] internals;
+        delete[] leaves;
+    }
 }
 
 
 void RadixTree::construct(int *buf) {
-    root = &internals[0];
-    for (int i = 0; i < count - 1; ++i) {
+#ifdef __CUDA_ARCH__
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    int delta = blockDim.x * gridDim.x;
+#else
+    int tid = 0;
+    int delta = 1;
+#endif
+
+    for (int i = tid; i < count; i += delta) {
+        leaves[i].val = buf[i];
+        leaves[i].left = leaves[i].right = nullptr;
+    }
+
+    for (int i = tid; i < count - 1; i += delta) {
         int d = (i == 0 ? 1 : lcp(buf[i], buf[i + 1]) - lcp(buf[i], buf[i - 1]));
         d = (d > 0 ? 1 : -1);
         int lcp_this = (i == 0 ? 0 : lcp(buf[i], buf[i - d]));
@@ -110,9 +135,13 @@ void RadixTree::construct(int *buf) {
 
 
 int RadixTree::lcp(int x, int y) {
+#ifdef __CUDA_ARCH__
+    return __clz(x ^ y);
+#else
     int res = 32;
     for (int z = x ^ y; z > 0; z >>= 1, --res);
     return res;
+#endif
 }
 
 
@@ -127,7 +156,7 @@ void RadixTree::printNode(RadixTreeNode *p) {
 
 
 void RadixTree::print() {
-    std::cout << "Tree:" << std::endl;
+    std::cout << "Tree: " << count << " elements" << std::endl;
     for (int i = 0; i < count - 1; ++i) {
         std::cout << "\t";
         printNode(&internals[i]);
@@ -157,7 +186,33 @@ bool RadixTree::check(RadixTreeNode *p, int l, int r) {
 }
 
 
-const int N = 100000, MAX = 10000000;
+/*
+__global__ void test(int *arr, int n, bool *res) {
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    RadixTree tree(arr, n);
+    __syncthreads();
+    if (tid == 0) {
+        *res = tree.check();
+    }
+}
+*/
+
+
+__global__  void init(RadixTree *tree, int n) {
+    tree->init(n);
+}
+__global__ void construct(RadixTree *tree, int *arr) {
+    tree->construct(arr);
+}
+__global__ void destroy(RadixTree *tree) {
+    tree->destroy();
+}
+__global__ void check(RadixTree *tree, bool *res) {
+    *res = tree->check();
+}
+
+
+const int N = 1000000, MAX = 10000000;
 int arr[N];
 
 int main() {
@@ -169,12 +224,42 @@ int main() {
         if (i == 0 || arr[i] != arr[i - 1])
             arr[n++] = arr[i];
     std::cout << n << std::endl;
+
+    bool res;
+
+    // CPU version
     /*
-    for (int i = 0; i < n; ++i) std::cout << arr[i] << " ";
-    std::cout << std::endl;
+    RadixTree tree;
+    tree.init(n);
+    tree.construct(arr);
+    res = tree.check();
+    tree.print();
+    tree.destroy();
     */
 
-    RadixTree tree(arr, n);
-    // tree.print();
-    std::cout << (tree.check() ? "Success" : "Failed") << std::endl;
+    // GPU version
+    int *arr_dev;
+    bool *res_dev;
+    RadixTree *tree_dev;
+    cudaMalloc(&arr_dev, n * sizeof(int));
+    cudaMalloc(&res_dev, sizeof(bool));
+    cudaMalloc(&tree_dev, sizeof(RadixTree));
+    cudaMemcpy(arr_dev, arr, n * sizeof(int), cudaMemcpyHostToDevice);
+
+    init<<<1, 1>>>(tree_dev, n);
+
+    int nblks = min(64, (n + N_THREADS_PER_BLK - 1) / N_THREADS_PER_BLK);
+    // test<<<nblks, N_THREADS_PER_BLK>>>(arr_dev, n, res_dev);
+    construct<<<nblks, N_THREADS_PER_BLK>>>(tree_dev, arr_dev);
+    check<<<1, 1>>>(tree_dev, res_dev);
+    cudaMemcpy(&res, res_dev, sizeof(bool), cudaMemcpyDeviceToHost);
+
+    destroy<<<1, 1>>>(tree_dev);
+    cudaDeviceSynchronize();
+    cudaCheckError();
+    cudaFree(arr_dev);
+    cudaFree(res_dev);
+
+    std::cout << (res ? "Success" : "Failed") << std::endl;
+    return 0;
 }
